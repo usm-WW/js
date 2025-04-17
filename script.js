b5169400e266b3d6078563c18c93d7cd0ac3e1e2
usm-WW <eldorbekanvarov4@gmail.com>
2025-04-17 17:19:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const sayName = prompt('Ismingizni kiriting')
+const sayAge = prompt('Yoshingizni kiriting')
+const sayAdd = prompt('Manzilingizni kiriting')
+text = 'Salom mening ismim ' + sayName + ' va men ' + sayAge + ' yoshdaman. Men ' + sayAdd + ' danman'
+alert(text)
